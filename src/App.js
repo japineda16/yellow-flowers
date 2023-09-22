7240f320d2160e1feac85e82580f1db1a7a4633f
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2023-09-21 20:33:27 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Lottie from 'lottie-react';
 import './App.css';
+import Flower from './assets/flower.json';
 
 function App() {
+  const queryParameters = new URLSearchParams(window.location.search)
+  const name = queryParameters.get("name")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='w-screen h-screen'>
+          <div className="bg-yellow-200 min-h-screen flex items-center justify-center">
+      <div className="p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">¡Feliz día, mi amor!</h1>
+        <p className="text-lg mb-4">
+          Espero que disfrutes estas flores amarillas como regalo especial para ti.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Lottie animationData={Flower} />
+        {name &&         <p className="text-sm mt-4">Con todo mi amor, {name}</p>}
+      </div>
+    </div>
     </div>
   );
 }
